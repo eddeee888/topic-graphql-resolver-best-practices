998721b2175ce9eb7e78606fb735427799913f83
Eddy Nguyen <ch@eddeee888.me>
2023-02-22 21:29:31 +1100

    Make data better

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,12 +1,14 @@
 import type { CatMapper } from "../schema/cat/schema.mappers";
 
-const createCat = (id: number): CatMapper => {
+const createCat = (
+  input: Partial<CatMapper> & Pick<CatMapper, "id">
+): CatMapper => {
   return {
-    id: `${id}`,
-    firstName: `firstName${id}`,
-    lastName: `lastName${id}`,
-    birthYear: 2020 + id,
-    isChipped: true,
+    id: `${input.id}`,
+    firstName: input.firstName || `firstName${input.id}`,
+    lastName: input.lastName || `lastName${input.id}`,
+    birthYear: input.birthYear || 2021,
+    isChipped: input.isChipped || true,
   };
 };
 
@@ -14,10 +16,10 @@ export const data: {
   cats: Record<string, CatMapper>;
 } = {
   cats: {
-    "1": createCat(1),
-    "2": createCat(2),
-    "3": createCat(3),
-    "4": createCat(4),
-    "5": createCat(5),
+    "1": createCat({ id: "1", firstName: "Catty", lastName: "McCatface" }),
+    "2": createCat({ id: "2", firstName: "single", lastName: "lonely_boi" }),
+    "3": createCat({ id: "3" }),
+    "4": createCat({ id: "4" }),
+    "5": createCat({ id: "5" }),
   },
 };
